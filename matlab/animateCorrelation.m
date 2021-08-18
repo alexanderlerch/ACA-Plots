988c85a12175497fb890a69cf0860a740cca5192
alexanderlerch <alexander.lerch@gatech.edu>
2021-08-17 22:36:09 -0400

    added two animations for Correlation added correlation plot added pca example

---
+++ matlab/animateCorrelation.m
@@ -0,0 +1,30 @@
+function animateCorrelation()
+
+    hFigureHandle = generateFigure(13.12,7);
+    
+    [cPath, cName]  = fileparts(mfilename('fullpath'));
+    cOutputPath = [cPath '/../graph/animation/' strrep(cName, 'animate', '')];
+
+    t       = 0:499;
+    rect    = [zeros(1,150) ones(1,50) zeros(1,300)];
+    tri     = [zeros(1,250) linspace(0,1,50) linspace(1,0,50) zeros(1,150)];
+    
+    r = zeros(1,251);
+    
+    for (k = 0:-1:-250)
+        subplot(211),plot(t,rect,t,[tri(-k+1:end),zeros(1,-k)])
+        axis([t(1) t(end) 0 1.1])
+        myarea = rect.*[tri(-k+1:end),zeros(1,-k)];
+        hold on;area(t,myarea);hold off;
+        legend('$x(t)$',['$y(t-' int2str(-k) ')$'],'mult.\ area')
+
+        subplot(212)
+        r(-k+1) = rect*[tri(-k+1:end),zeros(1,-k)]';
+        plot(-250:0,fliplr(r))
+        axis([-250 0 0 40])
+        ylabel('$r_{xy}$')
+        xlabel('$\eta$')
+
+        printFigure(hFigureHandle, [cOutputPath '-' num2str(-k,'%.3d')]); 
+    end    
+end
---
+++ matlab/plotPcaExample.m
@@ -0,0 +1,80 @@
+function displayPcaExample()
+
+    if (exist('ComputeFeature') ~= 2)
+        error('Please add the ACA scripts (https://github.com/alexanderlerch/ACA-Code) to your path!');
+    end
+
+    hFigureHandle = generateFigure(13.12,7);
+    
+    [cPath, cName]  = fileparts(mfilename('fullpath'));
+    cOutputFilePath = [cPath '/../graph/' strrep(cName, 'plot', '')];
+    cAudioPath = [cPath '/../audio/'];
+    cName = 'sax_example.wav';
+
+    % generate plot data
+    [v,score,latent,cFeatureNames] = getData([cAudioPath,cName]);
+
+    % plot
+    subplot(2,2,1)
+    plot(v'), 
+    grid on, 
+    %legend(cFeatureNames,'Location','SouthOutside')
+    xlabel('$n$')
+    ylabel('$v(n)$')
+    axis([1 size(v,2) min(min(v)) max(max(v))])
+    %printFigure(hFigureHandle, [cOutputFilePath '_input'])
+ 
+    score = score(:,1:2);
+    subplot(222)
+    plot(score), 
+    grid on
+    xlabel('$n$')
+    ylabel('$pc(n)$')
+    axis([1 size(v,2) min(min(score)) max(max(score))])
+    
+    subplot(212)
+    plot(latent), 
+    grid on
+    hold on; 
+    plot(ones(1,size(v,1)),'Color',[.6 .6 .6]); 
+    hold off;
+    xlabel('component')
+    ylabel('eigenvalue');
+    set(gca,'XTick', 1:length(latent))
+    axis([1 length(latent) 0 max(latent)+.1 ])
+    printFigure(hFigureHandle, cOutputFilePath)
+
+end
+
+function [v,score, latent,cFeatureNames] = getData(cInputPath)
+
+    cFeatureNames = char('SpectralCentroid',...
+        'SpectralRolloff',...
+        'SpectralSpread',...
+        'TimeZeroCrossingRate',...
+        'TimePeakEnvelope');
+
+    iNumBlocks = 500;
+    
+    % read sample data
+    [x,fs] = audioread(cInputPath);
+    x       = x/max(abs(x));
+
+    [tmp, t] = ComputeFeature (deblank(cFeatureNames(1,:)), x, fs);
+    v = tmp(:,1:iNumBlocks);
+    for (i = 2:size(cFeatureNames,1))
+        [tmp, t] = ComputeFeature (deblank(cFeatureNames(i,:)), x, fs);
+        v = [v;tmp(1,1:iNumBlocks)];
+    end
+    
+    %norm
+    m = median(v,2);
+    for (i = 1:size(v,1))
+        v(i,:)  = v(i,:) - m(i);
+        s       = sqrt(v(i,:)*v(i,:)'/length(v));
+        v(i,:)  = v(i,:) / s;
+    end
+    
+    [coeff, score, latent] = ToolPca(v');
+
+end
